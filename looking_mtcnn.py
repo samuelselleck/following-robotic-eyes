8be13eb09ac406bed253b9ee5563965dff328b54
Samuel Selleck <sellecksamuel@gmail.com>
2022-05-07 09:37:21 +0200

    mtcnn version

--- looking_mtcnn.py
+++ looking_mtcnn.py
@@ -1,22 +1,22 @@
 import serial
 import time
 import cv2
-
+from mtcnn.mtcnn import MTCNN
 debug_camera = True
 
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 video_capture = cv2.VideoCapture(0)
 
-# Loading the required haar-cascade xml classifier file, classifies face from non face
-haar_cascade = cv2.CascadeClassifier('face_recognition.xml')
+detector = MTCNN()
 
 while True:
     # Capture frame-by-frame
     ret, img = video_capture.read()
 
     # Converting image to grayscale, detection model sees it as grayscale
-    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    
+    faces = detector.detect_faces(img)
+    for face in faces:
+        print(face)
     # Applying the face detection method on the grayscale image
     #faces_rect = haar_cascade.detectMultiScale(gray_img, 1.2, 9)
     # Iterating through rectangles of detected faces, displaying the rectangle
